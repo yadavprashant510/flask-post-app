ba7aeecba942ee581d3a30bdce8a34311250729e
Prashant Yadav <prashant.yadav@terminus.com>
2024-04-07 19:09:44 +0530

    Extend the template using jinja

--- main.py
+++ main.py
@@ -24,5 +24,15 @@ def home():
     return render_template('home.html', posts=posts)
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html', posts=posts)
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', posts=posts)
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port=5000,)
