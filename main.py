316b4a15c43fce227cd48aa4e17a71e62c7df12d
Prashant Yadav <prashant.yadav@terminus.com>
2024-04-13 15:51:40 +0530

    Add login and registration page and modified the css class

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, url_for
+
+from forms import LoginForm, RegistrationForm
 
 app = Flask(__name__)
-
-# dummy post
+app.config['SECRET_KEY'] = '4e10bb420e7fe20190a0e99f35d55a25'
+# dummy poste
 posts = [
     {
         'author': 'Prashant Yadav',
@@ -27,6 +29,27 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', posts=posts)
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    else:
+        print("Else exe")
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.email.data == 'admin@email.com' and form.password.data == 'password':
+        flash("You have been logged in", 'success')
+    else:
+        flash("Login failed. Please check username and password", 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 @app.route('/contact')
